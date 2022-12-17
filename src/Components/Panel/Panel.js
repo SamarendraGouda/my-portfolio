248efc06cdb49db7a6a80a72ad7179c6ee5c9977
Samarendra Gouda <samarendragouda@gmail.com>
2022-12-17 17:26:01 +0530

    NavButtons Added

--- src/Components/Panel/Panel.js
+++ src/Components/Panel/Panel.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './Panel.module.css';
+import NavButtons from '../../HelperComponents/NavButtons/NavButtons';
 
 
 const Panel = () => {
 
     return (
         <div className={styles.container}>
-            
+            <div className={styles.navbar}>
+                <NavButtons fileName={"Home.jsx"} fileType={"jsx"}/>
+                <NavButtons fileName={"AboutMe.sol"} fileType={"sol"}/>
+                <NavButtons fileName={"ContactMe.js"} fileType={"js"}/>
+            </div>
         </div>
     );
 };
