d7d4da61e9fd050b136c2e7460f47148808e0c71
Samarendra Gouda <samarendragouda@gmail.com>
2023-12-30 20:10:02 +0530

    fix: reset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import './App.css';
-import TitleBar from './Components/TitleBar/TitleBar';
+import "./App.css";
 
 function App() {
-  return (
-    <TitleBar/>
-  );
+  return <></>;
 }
 
 export default App;
