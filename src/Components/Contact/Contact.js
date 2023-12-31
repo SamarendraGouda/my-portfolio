95e574bcb704cdc69a3710e6f0f30fabac3ed202
Samarendra Gouda <samarendragouda@gmail.com>
2023-12-31 14:08:55 +0530

    feat: links-added

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,15 +2,40 @@ import React from "react";
 import styles from "./Contact.module.css";
 
 const Contact = () => {
+  const handleEmail = () => {
+    window.open("mailto:samarendragouda@gmail.com");
+  };
+  const handleTwitter = () => {
+    window.open("https://twitter.com/0xSamarendra");
+  };
+  const handleLinkedIn = () => {
+    window.open("https://www.linkedin.com/in/samarendragouda/");
+  };
+  const handleTelegram = () => {
+    window.open("https://t.me/samarendrag");
+  };
+  const handleDiscord = () => {
+    window.open("https://discord.com/users/samarendrag");
+  };
+
   return (
     <div className={styles.container}>
-      {/* <div className={styles.contactHeading}>Contact Me</div> */}
       <div className={styles.contactList}>
-        <div className={styles.list}>Email</div>
-        <div className={styles.list}>Twitter</div>
-        <div className={styles.list}>LinkedIn</div>
-        <div className={styles.list}>Telegram</div>
-        <div className={styles.list}>Discord</div>
+        <div className={styles.list} onClick={() => handleEmail()}>
+          Email
+        </div>
+        <div className={styles.list} onClick={() => handleTwitter()}>
+          Twitter
+        </div>
+        <div className={styles.list} onClick={() => handleLinkedIn()}>
+          LinkedIn
+        </div>
+        <div className={styles.list} onClick={() => handleTelegram()}>
+          Telegram
+        </div>
+        <div className={styles.list} onClick={() => handleDiscord()}>
+          Discord
+        </div>
       </div>
     </div>
   );
